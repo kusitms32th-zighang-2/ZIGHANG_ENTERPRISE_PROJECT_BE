d701239e1c8a6f9c06857d9778c232bec8f0f619
dldusgh318 <pololydotoly@gmail.com>
2025-09-16 21:21:09 +0900

    feat: 테스트없이 온보딩하는 API 추가 (#51)

--- src/main/java/zighang2/zighang/web/controller/OnboardingController.java
+++ src/main/java/zighang2/zighang/web/controller/OnboardingController.java
@@ -2,10 +2,10 @@ package zighang2.zighang.web.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import zighang2.zighang.global.payload.ApiResponse;
 import zighang2.zighang.web.dto.OnboardingDto;
+import zighang2.zighang.web.dto.UserDto;
 import zighang2.zighang.web.service.OnboardingService;
 
 @RestController
@@ -15,21 +15,24 @@ public class OnboardingController {
 
     private final OnboardingService onboardingService;
 
-    @Operation(summary = "온보딩 캐릭터 발급 API", description = "온보딩 결과를 통해, 온보딩 캐릭터와 기업 규모, 복지 데이터를 제공하는 API입니다.")
+    @Operation(summary = "테스트 캐릭터 발급 API", description = "온보딩 결과를 통해, 온보딩 캐릭터와 기업 규모, 복지 데이터를 제공하는 API입니다.")
     @PostMapping("/character")
     public ApiResponse<OnboardingDto.OnboardingResponse> getOnboardingCharacter(@RequestBody OnboardingDto.OnboardingRequest request) {
         return ApiResponse.onSuccess(onboardingService.getOnboardingCharacter(request));
     }
 
-    @Operation(summary = "온보딩 이후 회원가입 API", description = "온보딩 이후, 유저의 회원가입을 하는 API입니다.")
-    @PostMapping("/signup")
+    @Operation(summary = "테스트 이후 회원가입 API", description = "테스트 이후, 유저의 회원가입을 하는 API입니다.")
+    @PostMapping("/test/signup")
     public ApiResponse<OnboardingDto.OnboardingSignupResponse> OnboardingSignup(@RequestBody OnboardingDto.OnboardingSignupRequest request) {
-        return ApiResponse.onSuccess(onboardingService.onboardingSignup(request));
+        return ApiResponse.onSuccess(onboardingService.onboardingSignupAfterTest(request));
 
     }
 
+    @Operation(summary = "테스트없이 회원가입 API", description = "테스트 없이, 유저의 회원가입을 하는 API입니다.")
+    @PostMapping("/signup")
+    public ApiResponse<UserDto.MypageResponseDto> OnboardingSignup(@RequestBody UserDto.MypageRequestDto request) {
+        return ApiResponse.onSuccess(onboardingService.onboardingSignup(request));
 
-
-
+    }
 
 }
